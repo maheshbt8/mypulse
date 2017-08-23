cb7bdcdfdc6091fb870a3a091664308c0b62a3b1
yusuf128 <mymirza127@gmail.com>
2017-08-22 22:36:21 -0700

    nurse module completed

--- application/controllers/Nurse.php
+++ application/controllers/Nurse.php
@@ -196,43 +196,6 @@ class Nurse extends CI_Controller {
     }
 
 
-    //     public function getDTPatient() {
-    //     if ($this->auth->isLoggedIn() && ($this->auth->isNurse())) {
-    //             $uid = $this->auth->getuserid();
-               
-    //            $patients_ids =  $this->nurse_model->getpatient($uid);
-
-    //         $this->load->library("tbl");
-    //         $table = "hms_users";
-    //         $primaryKey = "id";
-    //         $columns = array(array("db" => "first_name", "dt" => 0, "formatter" => function ($d, $row) {
-    //             $user = $this->users_model->getusersById($row['id']);
-    //             $name = "";
-    //             if(isset($user['first_name'])){
-    //                 $name = $user['first_name'];
-    //             }
-    //             if(isset($user['last_name'])){
-    //                 $name .= " ".$user['last_name'];
-    //             }
-    //             return $name;
-    //         }), array("db" => "useremail", "dt" => 1, "formatter" => function ($d, $row) {
-    //             return "<a href='#' data-id='$row[id]' class='editbtn' data-toggle='modal' data-target='#edit' data-toggle='tooltip' title='Edit'>".$d."</a>";
-    //         }), array("db" => "id", "dt" => 2, "formatter" => function ($d, $row) {
-    //              return "<a href=\"#\" id=\"Patient_id\" class=\"InPatientBtn\"  data-toggle=\"modal\" data-target=\".bs-example-modal-sm\" data-id=\"$d\" data-toggle=\"tooltip\" title=\"Inpatient\"><i class=\"glyphicon glyphicon-log-in\"></i></button>";
-    //         }));
-    //         if(count($patients_ids) == 0){
-    //         $patients_ids[] = '-1';
-    //         }   
-    //        $strpatient_ids = implode(',',$patients_ids); 
-    //         $cond[] = 'id in ('.$strpatient_ids.')';           
-    //         $this->tbl->setTwID(implode(' AND ',$cond));
-    //         $this->tbl->setIndexColumn(true);
-    //         $this->tbl->setCheckboxColumn(false);
-    //         // SQL server connection informationhostname" => "localhost",
-    //         $sql_details = array("user" => $this->config->item("db_user"), "pass" => $this->config->item("db_password"), "db" => $this->config->item("db_name"), "host" => $this->config->item("db_host"));
-    //         echo json_encode($this->tbl->simple($_GET, $sql_details, $table, $primaryKey, $columns));
-    //     }
-    // }
   
         public function getDTPatient() {
         if ($this->auth->isLoggedIn() && ($this->auth->isNurse())) {
