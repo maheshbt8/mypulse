9638ad6cacfadb5dbf92c3a80ec8fda0fb8644bd
yogesh <patelyogeshawp@gmail.com>
2017-09-09 12:51:41 +0530

    dash drill

--- application/views/index/admindashboard.php
+++ application/views/index/admindashboard.php
@@ -6,7 +6,7 @@
     <div id="main-wrapper">
         <div class="row">
             <div class="col-lg-3 col-md-6">
-                <a href="">
+                <a href="<?php echo site_url();?>/branches">
                     <div class="panel info-box panel-white">
                         <div class="panel-body">
                             <div class="info-box-stats">
@@ -21,43 +21,49 @@
                 </a>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="panel info-box panel-white">
-                    <div class="panel-body">
-                        <div class="info-box-stats">
-                            <p class="counter"><?php echo $states['tot_doc'];?></p>
-                            <span class="info-box-title"><?php echo $this->lang->line('doctors');?></span>
-                        </div>
-                        <div class="info-box-icon">
-                            <i class="fa fa-user-md"></i>
+                <a href="<?php echo site_url();?>/doctors">
+                    <div class="panel info-box panel-white">
+                        <div class="panel-body">
+                            <div class="info-box-stats">
+                                <p class="counter"><?php echo $states['tot_doc'];?></p>
+                                <span class="info-box-title"><?php echo $this->lang->line('doctors');?></span>
+                            </div>
+                            <div class="info-box-icon">
+                                <i class="fa fa-user-md"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="panel info-box panel-white">
-                    <div class="panel-body">
-                        <div class="info-box-stats">
-                            <p><span class="counter"><?php echo $states['tot_pat'];?></span></p>
-                            <span class="info-box-title"><?php echo $this->lang->line('patients');?></span>
-                        </div>
-                        <div class="info-box-icon">
-                            <i class="fa fa-users"></i>
+                <a href="<?php echo site_url();?>/patients">
+                    <div class="panel info-box panel-white">
+                        <div class="panel-body">
+                            <div class="info-box-stats">
+                                <p><span class="counter"><?php echo $states['tot_pat'];?></span></p>
+                                <span class="info-box-title"><?php echo $this->lang->line('patients');?></span>
+                            </div>
+                            <div class="info-box-icon">
+                                <i class="fa fa-users"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="panel info-box panel-white">
-                    <div class="panel-body">
-                        <div class="info-box-stats">
-                            <p class="counter"><?php echo $states['tot_app'];?></p>
-                            <span class="info-box-title"><?php echo $this->lang->line('appointments');?></span>
-                        </div>
-                        <div class="info-box-icon">
-                            <i class="icon-envelope"></i>
+                <a href="<?php echo site_url();?>/appoitments">
+                    <div class="panel info-box panel-white">
+                        <div class="panel-body">
+                            <div class="info-box-stats">
+                                <p class="counter"><?php echo $states['tot_app'];?></p>
+                                <span class="info-box-title"><?php echo $this->lang->line('appointments');?></span>
+                            </div>
+                            <div class="info-box-icon">
+                              <i class="icon-envelope"></i>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div><!-- Row -->
         <div class="row">
--- application/views/index/superadmindashboard.php
+++ application/views/index/superadmindashboard.php
@@ -6,56 +6,64 @@
     <div id="main-wrapper">
     <div class="row">
         <div class="col-lg-3 col-md-6">
-            <div class="panel info-box panel-white">
-                <div class="panel-body">
-                    <div class="info-box-stats">
-                        <p class="counter"><?php echo $states['tot_hos'];?></p>
-                        <span class="info-box-title"><?php echo $this->lang->line('hospitals');?></span>
-                    </div>
-                    <div class="info-box-icon">
-                        <i class="fa fa-hospital-o"></i>
+            <a href="<?php echo site_url();?>/hospitals">
+                <div class="panel info-box panel-white">
+                    <div class="panel-body">
+                        <div class="info-box-stats">
+                            <p class="counter"><?php echo $states['tot_hos'];?></p>
+                            <span class="info-box-title"><?php echo $this->lang->line('hospitals');?></span>
+                        </div>
+                        <div class="info-box-icon">
+                            <i class="fa fa-hospital-o"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-lg-3 col-md-6">
-            <div class="panel info-box panel-white">
-                <div class="panel-body">
-                    <div class="info-box-stats">
-                        <p class="counter"><?php echo $states['tot_doc'];?></p>
-                        <span class="info-box-title"><?php echo $this->lang->line('doctors');?></span>
-                    </div>
-                    <div class="info-box-icon">
-                        <i class="fa fa-user-md"></i>
+            <a href="<?php echo site_url();?>/doctors">
+                <div class="panel info-box panel-white">
+                    <div class="panel-body">
+                        <div class="info-box-stats">
+                            <p class="counter"><?php echo $states['tot_doc'];?></p>
+                            <span class="info-box-title"><?php echo $this->lang->line('doctors');?></span>
+                        </div>
+                        <div class="info-box-icon">
+                            <i class="fa fa-user-md"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-lg-3 col-md-6">
-            <div class="panel info-box panel-white">
-                <div class="panel-body">
-                    <div class="info-box-stats">
-                        <p><span class="counter"><?php echo $states['tot_pat'];?></span></p>
-                        <span class="info-box-title"><?php echo $this->lang->line('patients');?></span>
-                    </div>
-                    <div class="info-box-icon">
-                        <i class="fa fa-users"></i>
+            <a href="<?php echo site_url();?>/patients">
+                <div class="panel info-box panel-white">
+                    <div class="panel-body">
+                        <div class="info-box-stats">
+                            <p><span class="counter"><?php echo $states['tot_pat'];?></span></p>
+                            <span class="info-box-title"><?php echo $this->lang->line('patients');?></span>
+                        </div>
+                        <div class="info-box-icon">
+                            <i class="fa fa-users"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-lg-3 col-md-6">
-            <div class="panel info-box panel-white">
-                <div class="panel-body">
-                    <div class="info-box-stats">
-                        <p class="counter"><?php echo $states['tot_app'];?></p>
-                        <span class="info-box-title"><?php echo $this->lang->line('appointments');?></span>
-                    </div>
-                    <div class="info-box-icon">
-                        <i class="icon-envelope"></i>
+            <a href="<?php echo site_url();?>/appoitments">
+                <div class="panel info-box panel-white">
+                    <div class="panel-body">
+                        <div class="info-box-stats">
+                            <p class="counter"><?php echo $states['tot_app'];?></p>
+                            <span class="info-box-title"><?php echo $this->lang->line('appointments');?></span>
+                        </div>
+                        <div class="info-box-icon">
+                            <i class="icon-envelope"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 <?php
